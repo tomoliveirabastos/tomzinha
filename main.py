6cfa24784900b753ec3fdcb1da0623e18270f430
Tom Oliveira <washington-kd@hotmail.com>
2023-12-25 18:11:00 -0300

    add

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-
-# from firmata_function import FirmataBoard
-# FirmataBoard().ligar('d:7:o')
-from vosk import Model, KaldiRecognizer
+import vosk
+import threading
 import pyfirmata2 as  firmata
 import pyaudio
 import pyttsx3
 import json
+import queue
+
+
+q = queue.Queue()
 
 f = open("env.json")
 content = f.read()
@@ -27,35 +29,54 @@ def desligarFn(frase: str):
 
        return False
 
-mic = pyaudio.PyAudio()
-PORT = 'COM5'
-board = firmata.Arduino(PORT)
+def worker1():
+       mic = pyaudio.PyAudio()
+       PORT = 'COM5'
+       board = firmata.Arduino(PORT)
+       stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192) 
+       stream.start_stream()
+       model = vosk.Model(j["caminho"])
+       recognizer = vosk.KaldiRecognizer(model, 16000) 
+       LED_pin = board.get_pin("d:7:o")  # Initialize the pin (d => digital, 13 => Number of the pin, o => output)
 
-stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192) 
-stream.start_stream()
+       q.put("Olá, eu sou a Tomzinha")
 
-model = Model(j["caminho"])
-recognizer = KaldiRecognizer(model, 16000) 
+       while True:
+              try:
+                     data = stream.read(4096)
+                     if recognizer.AcceptWaveform(data):
+                            text = recognizer.Result()
+                            t = text[14:-3]
+                            print(t)
+                            
+                            if t.strip() != "":
+                                   if ligarFn(t):
+                                          LED_pin.write(False)
+                                          q.put("O comando de ligar foi acionado.")
+                                          
+                                   elif desligarFn(t):
+                                          LED_pin.write(True)
+                                          q.put("O comando de desligar foi acionado.")
 
-LED_pin = board.get_pin("d:7:o")  # Initialize the pin (d => digital, 13 => Number of the pin, o => output)
-
-while True:
-
-       try:
-              data = stream.read(4096)
-              if recognizer.AcceptWaveform(data):
-                     text = recognizer.Result()
-                     t = text[14:-3]
-
-                     print(t)
+                                   else:
+                                          q.put("Eu não entendi o comando, pode repetir ?")
+                                   
+              except Exception as err:
+                     q.put("Ocorreu um erro inesperado, o sistema foi reiniciado")
+                     print(err)
                      
-                     if ligarFn(t):
-                            LED_pin.write(False)
-              
-                     elif desligarFn(t):
-                            LED_pin.write(True)
-  
-       except Exception as err:
-              print(err)
-                     
-                     
+def worker2():
+       engine = pyttsx3.init()
+       
+       while True:
+              a = q.get()
+              engine.say(a)
+              engine.runAndWait()
+
+q.join()
+
+t1 = threading.Thread(target=worker1)
+t2 = threading.Thread(target=worker2)
+
+t1.start()
+t2.start()
